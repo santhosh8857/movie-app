efcdc3ef57e3784879d9ead9330033c469262e46
Santhosh T <santhosh8857@gmail.com>
2024-02-17 14:55:35 +0530

    Update Footer.js

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="footer-content">
       <footer>
-        <p>SHOW TIME &copy; 2022 All Rights Reversed</p>
+        <p>SHOW TIME &copy; 2024 All Rights Reversed</p>
         <p className="contact">For any queries, contact : +91 8667802699</p>
       </footer>
     </div>
